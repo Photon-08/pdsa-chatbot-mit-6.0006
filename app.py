3a014c91f1d8e4d1ff6bd11f59d3f5a53e4fca72
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2024-02-25 23:56:01 +0530

    added 2nd llm: Flan large

--- app.py
+++ app.py
@@ -26,6 +26,17 @@ qa_chain = RetrievalQA.from_chain_type(
 )
 
 
+llm_2 = HuggingFaceHub(
+    repo_id="google/flan-t5-large", model_kwargs={"temperature": 0.5, "max_length": 10000000},
+    huggingfacehub_api_token="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"
+)
+
+qa_chain_2 = RetrievalQA.from_chain_type(
+    llm_2,
+    retriever=vectordb.as_retriever()
+)
+
+
 
 st.header("Chatbot for IIT Madras BS Degree: Ask questions to 6.006 Intro to Algorithms by MIT")
 st.caption("Developed by Indranil Bhattacharyya (21F1005840)")
@@ -58,13 +69,18 @@ if prompt:
     with st.spinner('Wait for it...'):
     
         result = qa_chain({"query":prompt})
+        result_2 = qa_chain_2({"query":prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
     with st.chat_message('assistant'):
-        ans = result["result"]
-        ans = ans[163:]
-        st.write(ans)
+        ans_1 = result["result"]
+        ans_1 = ans_1[163:]
+        st.write(ans_1)
+        st.write("--------------")
+        st.write("Response from Model 2":)
+        ans_2 = result_2['result']
+        st.write(ans_2)
 
     key_name = 'question' + str(c)
     st.session_state[prompt] = result["result"]
